616e3bb9d2ac00455490ec22ddd3b8de03cf7bb3
jacekelgda <jacekelgda@gmail.com>
2015-03-05 16:35:53 +0100

    mysql configuration

--- www/index.php
+++ www/index.php
@@ -1,5 +1,16 @@
 <?php
+echo "<pre>";
+echo getenv('MYSQL_PORT_3306_TCP_ADDR');
+echo "</pre>";
 
-phpinfo();
+$link = mysqli_connect("172.17.0.13:3306","wordpress","Passw0rd","wordpress") or die("Error " . mysqli_error($link));
+
+$query = "SELECT name FROM mytable" or die("Error in the consult.." . mysqli_error($link));
+
+$result = $link->query($query);
+
+var_dump($result);
+
+// phpinfo();
 
 ?>
