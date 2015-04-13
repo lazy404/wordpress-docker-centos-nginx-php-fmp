ac31679c1d5fe09c91c8a38c1bffad0a69d3bebd
jacekelgda <jacekelgda@gmail.com>
2015-04-13 09:36:09 +0200

    change wp install

--- docker/images/php/container-files/install-wp.sh
+++ docker/images/php/container-files/install-wp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd /var/www
-composer install -n
+if [ ! -d "/var/www/wp" ]; then
+  composer install -n
+fi
 rm -fr /var/www/wp/wp-content
 cd /var/www/wp
 ln -s /var/www/wp-content/ wp-content
