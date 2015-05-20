2c144d1f2bda1fab8b499a5fc18ae43ded91a1a1
jacekelgda <jacekelgda@gmail.com>
2015-05-20 15:32:18 +0200

    improve usage of MYSQL container port

--- docker/images/php/container-files/config/init/10-nginx-cache-dirs.sh
+++ docker/images/php/container-files/config/init/10-nginx-cache-dirs.sh
@@ -12,7 +12,12 @@ mkdir -p /run/user/nginx-cache
 mkdir -p /run/user/nginx-cache-tmp
 chown -R www:www /run/user/nginx-cache*
 
-echo fastcgi_param MYSQL_PORT_3306_TCP_ADDR $MYSQL_PORT_3306_TCP_ADDR';' >> /etc/nginx/fastcgi_params
+if [ ! "$MYSQL_PORT_3306_TCP_ADDR" ]; then
+    echo fastcgi_param MYSQL_PORT_3306_TCP_ADDR $MYSQL_PORT_3306_TCP_ADDR';' >> /etc/nginx/fastcgi_params
+else
+    echo fastcgi_param MYSQL_PORT_3306_TCP_ADDR foo';' >> /etc/nginx/fastcgi_params
+fi
+
 echo fastcgi_param RDS_HOSTNAME $RDS_HOSTNAME';' >> /etc/nginx/fastcgi_params
 echo fastcgi_param RDS_DB_NAME $RDS_DB_NAME';' >> /etc/nginx/fastcgi_params
 echo fastcgi_param RDS_PASSWORD $RDS_PASSWORD';' >> /etc/nginx/fastcgi_params
